711d375234f0ba331b8b07a0bbb25b5fc6314602
ZairaStra <straticozaira@gmail.com>
2025-07-01 16:38:29 +0200

    entitymanager

--- src/main/java/zairastra/Application.java
+++ src/main/java/zairastra/Application.java
@@ -1,8 +1,20 @@
 package zairastra;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import zairastra.dao.EventsDAO;
+
 public class Application {
+    //FAI ATTENZIONE: DEVE CHIAMARSI ESATTAMENTE COME è SCRITTO NEL PERSISTENCE.XML
+    private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("u4w3d2pu")
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em= emf.createEntityManager(); //è LUI A GESTIRE LE RELAZIONI COL DB TRAMITE I METODI IN EVENTSDAO
+        EventsDAO ed=new EventsDAO(em);
+
+        //RICORDATI DI CHIUDERE DOPO CHE LO USI
+        em.close();
+        emf.close();
     }
 }
--- src/main/java/zairastra/dao/EventsDAO.java
+++ src/main/java/zairastra/dao/EventsDAO.java
@@ -1,4 +1,17 @@
 package zairastra.dao;
 
+import jakarta.persistence.EntityManager;
+
 public class EventsDAO {
+    //ENTITYMANAGER SERVE A GESTIRE I METODI TUTTI NEL DAO PASSANDOLA COME PARAMETRO IN MODO DA LASCIARE PULITO IL MAIN
+    //E NON RISCRIVERE I METODI OGNI VOLTA CHE VOGLIO RICHIAMARLI SU UN ELEMENTO DELLA TABELLA
+    private EntityManager entityManager;
+
+    public EventsDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void save(Event newEvent) {
+
+    }
 }
